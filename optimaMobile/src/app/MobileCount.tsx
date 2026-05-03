7c32dc9e96b4e2da856f7b7012d28bf1c23f492b
ahmedr00 <jibounani123@gmail.com>
2026-05-03 10:14:16 +0100

    none

--- optimaMobile/src/app/MobileCount.tsx
+++
@@ -1,9 +0,0 @@
-import { ScrollView, Text } from "react-native";
-
-export default function MobileCount() {
-  return (
-    <ScrollView>
-      <Text>Mobile Count</Text>
-    </ScrollView>
-  );
-}
